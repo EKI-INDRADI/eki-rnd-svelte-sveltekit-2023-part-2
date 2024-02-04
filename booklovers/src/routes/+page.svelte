5bdd8c60c68f15b5f5cdf481e2bdd084f10d22ae
EKI-INDRADI <eki.indradi@gmail.com>
2024-02-04 21:39:02 +0700

    EKI-20240204-065-Firebase-initialize-on-the-server

--- booklovers/src/routes/+page.svelte
+++ booklovers/src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!-- <h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
+
+
+
+<script>
+    export let data;
+</script>
+
+<!-- data.count f12 view source (render on server side) -->
+<h1>Num of users: {data.count}</h1> 
